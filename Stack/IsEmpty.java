a1550fd7e5d6e46849bd01ceff69e6b6974aecb5
Shwetha <koppashwethav@gmail.com>
2026-07-17 13:37:10 +0530

    Stack Problems

--- Stack/IsEmpty.java
+++ Stack/IsEmpty.java
@@ -2,21 +2,21 @@ package Stack;
 
 public class IsEmpty 
 {
-   int[] stack =new int[5];
+   int[] stack = new int[5];
    int top = -1;
    
    public void push(int value)
    {
-    if(top == stack.length-1)
+       if(top == stack.length-1)
     {
        System.out.println("Stack Overflow");
        return;
     }
-    stack[++top] = value;
+       stack[++top] = value;
    }
-   public void IsEmpty()
-   {
-     if(top == -1)
+     public void IsEmpty()
+     {
+        if(top == -1)
      {
         System.out.println("Stack is empty");
      }
