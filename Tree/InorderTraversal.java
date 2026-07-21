c879c96651cae943861c7af75e07d0b144438ab3
Shwetha <koppashwethav@gmail.com>
2026-07-21 13:54:18 +0530

    Tree Problems

---
+++ Tree/InorderTraversal.java
@@ -0,0 +1,37 @@
+package Tree;
+
+public class InorderTraversal 
+{
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right =null;
+        }
+    }
+    public static void inorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static void main(String[] args) 
+    {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        inorder(root);
+    }
+}
