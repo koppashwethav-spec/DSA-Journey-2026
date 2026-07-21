1173d152be4800c76d2afc98c6481d50f0a4d493
Shwetha <koppashwethav@gmail.com>
2026-07-21 13:43:34 +0530

    Tree Problems

---
+++ Tree/PreorderTraversal.java
@@ -0,0 +1,38 @@
+package Tree;
+
+public class PreorderTraversal
+{
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right =null;
+        }
+    }  
+    public static void preorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args)
+    {
+        Node root =new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        preorder(root);
+    }
+}
