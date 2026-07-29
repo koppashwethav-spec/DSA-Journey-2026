2a68682a0b4cca811bccf9801ebe403006fcb5f7
Shwetha <koppashwethav@gmail.com>
2026-07-29 11:14:32 +0530

    Graph Problems

---
+++ Graph/DFS.java
@@ -0,0 +1,51 @@
+package Graph;
+
+import java.util.ArrayList;
+
+public class DFS {
+
+    public static void dfs(ArrayList<ArrayList<Integer>> graph, int node, boolean[] visited) {
+
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int neighbor : graph.get(node)) {
+
+            if (!visited[neighbor]) {
+                dfs(graph, neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int vertices = 5;
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+
+        graph.get(0).add(2);
+        graph.get(2).add(0);
+
+        graph.get(1).add(3);
+        graph.get(3).add(1);
+
+        graph.get(2).add(4);
+        graph.get(4).add(2);
+
+        graph.get(3).add(4);
+        graph.get(4).add(3);
+
+        boolean[] visited = new boolean[vertices];
+
+        System.out.print("DFS Traversal: ");
+
+        dfs(graph, 0, visited);
+    }
+}
