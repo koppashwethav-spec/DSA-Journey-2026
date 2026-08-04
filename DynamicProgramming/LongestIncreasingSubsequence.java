f88079d0f090456973eb3acfe4be94e7c786cc39
Shwetha <koppashwethav@gmail.com>
2026-08-04 13:06:17 +0530

    DYnamic Programming Problems

---
+++ DynamicProgramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,39 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+    public static int lis(int[] nums) {
+
+        int n = nums.length;
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+
+        System.out.println("Length of LIS = " + lis(nums));
+    }
+}
